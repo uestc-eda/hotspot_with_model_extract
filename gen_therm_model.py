6315e5a7dcf6c4c3156e814bebf2c0a71181b092
wanghaiuestc <wanghai@uestc.edu.cn>
2025-07-21 17:07:48 +0800

    update

---
+++ gen_therm_model.py
@@ -0,0 +1,6 @@
+
+def gen_therma_model():
+    config_file = 'DDR_16core/cores_hotspot.config'
+    power_file = 'power_core.trace'
+    model_secondary = 1 # 1 or 0
+    grid_layer_file = 'DDR_16core/cores.lcf'
